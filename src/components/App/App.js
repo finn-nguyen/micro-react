50b2b51bb01e67ffb4dce34c0b29c29961c1876c
Tuong <nvtuongcs@gmail.com>
2018-06-02 10:05:49 +0700

    Config eslint

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import './App.scss';
+import React from 'react'
+import './App.scss'
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  render () {
     return (
-      <div styleName="App">
-        <header styleName="App-header">
-          <img src='/images/logo.svg' styleName="App-logo" alt="logo" />
-          <h1 styleName="App-title">Welcome to React</h1>
+      <div styleName='App'>
+        <header styleName='App-header'>
+          <img src='/images/logo.svg' styleName='App-logo' alt='logo' />
+          <h1 styleName='App-title'>Welcome to React</h1>
         </header>
-        <p styleName="App-intro">
+        <p styleName='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
